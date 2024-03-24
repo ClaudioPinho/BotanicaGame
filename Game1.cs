27d70bd8f4e0e0b3634ad59d86e78599fc671ed9
Claudio Pinho <claudio.pinho@outlook.pt>
2024-03-24 20:43:11 +0000

    Added rest of the code

---
+++ Game1.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TestMonoGame;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private SpriteFont _fontSprite;
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        _fontSprite = Content.Load<SpriteFont>("Fonts/myFont");
+        Debug.WriteLine(_fontSprite.Characters.Count);
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        _spriteBatch.Begin();
+
+        var helloString = "Hello World, I am a pretty string!";
+        Vector2 textMiddlePoint = _fontSprite.MeasureString(helloString) / 2;
+        Vector2 textPosition = new Vector2(Window.ClientBounds.Width / 2, Window.ClientBounds.Height / 2);
+
+
+        // TODO: Add your drawing code here
+        _spriteBatch.DrawString(_fontSprite, helloString, textPosition, Color.Red, 0, textMiddlePoint, 1.0f, SpriteEffects.None, 0.5f);
+
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
